39818aa72362ed9d970db5282b489209dbb0f7b5
Ben <50995624+bcalabrese15@users.noreply.github.com>
2019-07-21 10:14:31 -0400

    Now reads equations
    Can now read simple 1 instance addition equations (ex: 2+2, 100+100)
    Can't yet read multiple instance equations (ex: 2+2+2)
    Can't do subtraction, multiplication, or division yet.
    Learning a lot about classes and how arguments work
    Don't like the indentation requirements though

--- calculator.py
+++ calculator.py
@@ -1,9 +1,10 @@
-input1 = input('Enter a number')
-num1 = int(input1)
-input2 = input('Enter a second number')
-num2 = int(input2)
-num1 += num2
-print(str(num1))
-
-
-
+class addition:
+	def _init_(self, num1, num2):
+		self.num1 = num1
+		self.num2 = num2
+	def add(num1, num2):
+		return num1 + num2
+input = input('Enter your calculation: ')
+if "+" in input:
+	result = addition.add(int(input[:input.find('+')]), int(input[input.find('+'):])) 
+	print(str(result))
