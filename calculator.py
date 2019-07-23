0be486b3b8a9f70c2557a1abda838088628b3e7d
Ben <50995624+bcalabrese15@users.noreply.github.com>
2019-07-22 20:18:32 -0400

    7/22/19
    This doesn't work but I'm stuck and need to commit something today

--- calculator.py
+++ calculator.py
@@ -4,7 +4,40 @@ class addition:
 		self.num2 = num2
 	def add(num1, num2):
 		return num1 + num2
+addition()
+class subtraction:
+	def _init_(self, num1, num2):
+		self.num1 = num1
+		self.num2 = num2
+	def sub(num1, num2):
+		return num1 + num2
+subtraction()
+class multiplication:
+	def _init_(self, num1, num2):
+		self.num1 = num1
+		self.num2 = num2
+	def mult(num1, num2):
+		return num1 * num2
+multiplication()
+class division:
+	def _init_(self, num1, num2):
+		self.num1 = num1
+		self.num2 = num2
+	def div(num1, num2):
+		return num1 / num2
+division()
 input = input('Enter your calculation: ')
-if "+" in input:
-	result = addition.add(int(input[:input.find('+')]), int(input[input.find('+'):])) 
-	print(str(result))
+for letter in input:
+	if "+" in input:
+		result = addition.add(int(input[:input.find('+')]), int(input[input.find('+'):])) 
+		print(str(result))
+	if "-" in input:
+		plusOne = int(input.find('-'))
+		result = subtraction.sub(int(input[:input.find('-')]), int(input[input.find('-'):plusOne])) 
+		print(str(result))
+	if "*" in input:
+		result = multiplication.mult(int(input[:input.find('*')]), int(input[input.find('*'):])) 
+		print(str(result))
+	if "/" in input:
+		result = division.div(int(input[:input.find('/')]), int(input[input.find('/'):])) 
+		print(str(result))
